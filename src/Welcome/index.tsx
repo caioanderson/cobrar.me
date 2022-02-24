e7e5c5661ad22396f82db21533e50315a22bfbac
caioanderson <caioanderson@trixlog.com>
2022-02-23 21:54:27 -0300

    first commit

---
+++ src/Welcome/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+interface WelcomeProps{
+    text: string;
+}
+
+export function Welcome({ text }: WelcomeProps) {
+    return (
+        <View style={styles.container}>
+            <Text>{text}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
